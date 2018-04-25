a29beeafb6be83b17d5f57ede074cb8543786d71
chunchaolee <chunchaolee@gmail.com>
2018-04-25 21:01:58 +0800

    setup User and Post relational database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+  # 一個post只屬於一個user
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # 一個user可以有很多筆posts資訊
+  has_many :posts, dependent: :destroy
+
 end
