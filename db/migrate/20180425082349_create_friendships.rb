5bff9454151c10a55a1e7cf8230007d4b2a9b881
chunchaolee <chunchaolee@gmail.com>
2018-04-25 16:38:44 +0800

    created friendships table

--- db/migrate/20180425082349_create_friendships.rb
+++ db/migrate/20180425082349_create_friendships.rb
@@ -1,6 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration[5.1]
   def change
     create_table :friendships do |t|
+      t.integer :apply_friend_id
+      t.integer :friend_id, :index => true
+      t.integer :user_id, :index => true
 
       t.timestamps
     end
