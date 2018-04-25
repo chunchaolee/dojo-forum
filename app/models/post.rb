5538c7c44e666e343e8f9f3b27e36980a189b533
chunchaolee <chunchaolee@gmail.com>
2018-04-25 21:32:42 +0800

    setup Post and Reply relational database

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
   # 一個post只屬於一個user
   belongs_to :user
 
+  # 一個post有很多replies
+  has_many :replies, dependent: :destroy
+
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,4 +3,7 @@ class Reply < ApplicationRecord
   # 一筆reply只屬於一個user
   belongs_to :user
 
+  # 一筆reply只屬於一個post
+  belongs_to :post
+
 end
