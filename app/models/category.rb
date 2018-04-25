e5327c347df13fa2062e949622fdd19582ab77ac
chunchaolee <chunchaolee@gmail.com>
2018-04-25 22:35:23 +0800

    setup Category and Post_category relational database

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+  # 一個category可以有很多個Post_categories
+  has_many :post_categories, dependent: :destroy
+
 end
--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -3,4 +3,7 @@ class PostCategory < ApplicationRecord
   # 一筆post_categories只屬於一筆post
   belongs_to :post
 
+  # 一筆post_categories只歸類於一個category
+  belongs_to :category
+
 end
