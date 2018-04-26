fb4943ba0ca492ec6e344f1c3e74a930a906c85f
chunchaolee <chunchaolee@gmail.com>
2018-04-26 17:57:34 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # setup user
+  resources :users, only: [:show, :edit]
+
+  # setup post
+  resources :posts do
+
+    resources :replies, only: [:create, :destroy]
+
+    collection do
+      get :feeds
+    end
+
+  end
+
+  # setup draft
+  resources :drafts
+
+  # setup friendship
+  resources :friendships, only: [:create, :destroy]
+
+  # admin
+  namespace :admin do
+
+    resources :categories
+    resources :users
+
+  end
+
+  # feed
+
+
+  root "posts#index"
+
 end
