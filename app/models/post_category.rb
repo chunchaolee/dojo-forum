0daa32e1ba0a8d8ab9466f88d27e9ae218e7ed36
chunchaolee <chunchaolee@gmail.com>
2018-04-26 18:20:20 +0800

    setup Post_category and Draft relational-database

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -3,6 +3,9 @@ class PostCategory < ApplicationRecord
   # 一筆post_categories只屬於一筆post
   belongs_to :post
 
+  # 一筆post_categories只屬於一筆post
+  belongs_to :draft
+
   # 一筆post_categories只歸類於一個category
   belongs_to :category
 
