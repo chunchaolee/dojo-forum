8d380daa6e98fce722abf951ea8e6aa302154138
chunchaolee <chunchaolee@gmail.com>
2018-04-26 21:48:11 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # setup user
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit] do 
+
+    # setup draft
+    resources :drafts, except: [:new]
+
+  end
 
   # setup post
   resources :posts do
 
-    resources :replies, only: [:create, :destroy]
+    resources :replies, only: [:create, :edit, :update, :destroy]
 
     collection do
       get :feeds
@@ -16,17 +21,20 @@ Rails.application.routes.draw do
 
   end
 
-  # setup draft
-  resources :drafts
-
   # setup friendship
   resources :friendships, only: [:create, :destroy]
 
   # admin
   namespace :admin do
 
-    resources :categories
-    resources :users
+    resources :categories, only: [:index, :create, :edit, :update, :destroy]
+    resources :users , only: [:index, :destroy] do 
+
+      member do
+        post :level
+      end
+
+    end
 
   end
 
