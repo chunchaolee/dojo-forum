8b5f655df8da7205d451687d6bf0bf21120317b6
chunchaolee <chunchaolee@gmail.com>
2018-04-26 21:56:46 +0800

    created collections table

--- db/migrate/20180426135258_create_collections.rb
+++ db/migrate/20180426135258_create_collections.rb
@@ -1,6 +1,8 @@
 class CreateCollections < ActiveRecord::Migration[5.1]
   def change
     create_table :collections do |t|
+      t.integer :user_id, :index => true
+      t.integer :post_id, :index => true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426095811) do
+ActiveRecord::Schema.define(version: 20180426135258) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_collections_on_post_id"
+    t.index ["user_id"], name: "index_collections_on_user_id"
   end
 
   create_table "drafts", force: :cascade do |t|
