649cf5361a283874bb7e2360f3ab47c6b98cd6ae
chunchaolee <chunchaolee@gmail.com>
2018-04-27 17:46:51 +0800

    setup admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# admin 帳號
+User.create!(email: "admin@example.com", name: "Admin", password: "12345678", is_admin: true)
+puts "Default admin created!"
