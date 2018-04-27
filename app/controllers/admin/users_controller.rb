53dda07e17023b365f654b7c069b4d3ce82cac19
chunchaolee <chunchaolee@gmail.com>
2018-04-27 17:50:49 +0800

    setup index method in admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,23 @@
 class Admin::UsersController < Admin::BaseController
+
+  ALLOW_QUERIES = %w[s name_cont email_cont is_admin_eq].freeze
+
+  def index
+    # ransack
+    @q = User.ransack(ransack_params)
+    @users = @q.result(distinct: true)
+  end
+
+  private 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def ransack_params
+    if params[:q].present?
+      params.require(:q).permit(*ALLOW_QUERIES)
+    end
+  end
+
 end
