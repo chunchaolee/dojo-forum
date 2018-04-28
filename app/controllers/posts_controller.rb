786c6554245ec1d47f3179da41b1851c5eebba6a
chunchaolee <chunchaolee@gmail.com>
2018-04-28 14:41:55 +0800

    setup index method in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,9 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :except => [:index]
   
+  def index
+    @categories = Category.all
+    @posts = Post.all
+  end
 
 end
