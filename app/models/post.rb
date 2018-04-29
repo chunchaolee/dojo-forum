cd041674f404891d2bec55fb067661cad8f7418b
chunchaolee <chunchaolee@gmail.com>
2018-04-29 18:09:29 +0800

    setup has_and_belongs_to_many method in post model and category model, added img uploader to post model also

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,18 @@
 class Post < ApplicationRecord
 
+  mount_uploader :img, PhotoUploader
+
   # 一個post只屬於一個user
-  belongs_to :user
+  belongs_to :user, optional: true
 
   # 一個post有很多replies
   has_many :replies, dependent: :destroy
 
   # 一個post可以有很多post_categories
-  has_many :post_categories, dependent: :destroy
+  # has_many :post_categories, dependent: :destroy
   # 一個post可以有很多category
-  has_many :categories, through: :post_categories, source: :category
+  # has_many :categories, through: :post_categories, source: :category
+  has_and_belongs_to_many :categories
 
   # 一個post可以有很collection
   has_many :collections, dependent: :destroy
