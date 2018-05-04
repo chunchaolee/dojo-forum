2fdd8eca7a0bd69c2cefb0daeade987e7f5fadab
chunchaolee <chunchaolee@gmail.com>
2018-05-05 00:14:58 +0800

    modified category and user model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,7 @@ class Category < ApplicationRecord
 
   validates_presence_of :name
 
-  # 一個category可以有很多個Post_categories
-  # has_many :post_categories
   # 一個category可以有很多post
-  # has_many :posts, through: :post_categories, source: :post
   has_and_belongs_to_many :posts
 
 end
