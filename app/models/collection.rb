872a1bf484ad6c30238dd514e323d4056f9c24c1
chunchaolee <chunchaolee@gmail.com>
2018-05-05 14:58:23 +0800

    added validates and option in collect controller

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,9 +1,12 @@
 class Collection < ApplicationRecord
 
+  # 每個user只能collect一個post一次
+  validates :post_id, uniqueness: { scope: :user_id }
+
   # 一個collection只屬於一個user
   belongs_to :user
 
   # 一個collection只對應一筆post
-  belongs_to :post
+  belongs_to :post, optional: true
 
 end
