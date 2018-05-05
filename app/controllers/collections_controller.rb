031c8bd51257ffbd1bcb23aa38982567bb872857
chunchaolee <chunchaolee@gmail.com>
2018-05-05 15:03:34 +0800

    setup create and destroy action in collections controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,4 +2,15 @@ class CollectionsController < ApplicationController
 
   before_action :authenticate_user!
 
+  def create
+    @collection = current_user.collections.create!(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @collection = current_user.collections.where(post_id: params[:id]).first
+    @collection.destroy
+  end
+
 end
