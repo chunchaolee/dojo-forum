ced208fbdf1591c35cacf59e8c14593b673e7d72
chunchaolee <chunchaolee@gmail.com>
2018-05-08 01:09:03 +0800

    added methods and relational db

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,14 @@ class User < ApplicationRecord
 
   # 一個user可以提出很多筆friendships
   has_many :friendships, dependent: :destroy
-  # 一個user可以接收到很多筆friendships
-  has_many :apply_friends, through: :friendships
   # 一個user可以有很多friend
   has_many :friends, through: :friendships
+
+  # 一個user可以收到很多筆friendships邀請
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  # 一個user可以有很多申請成為friend的user
+  has_many :appliers, through: :inverse_friendships, source: :user
+
 
   # 一個user可以有很多collections
   has_many :collections, dependent: :destroy
@@ -30,5 +34,12 @@ class User < ApplicationRecord
     self.collected_posts.include?(post)
   end
 
+  def is_friend?(user)
+    self.friends.include?(user) && user.friends.include?(self)
+  end
+
+  def is_applied?(user)
+    self.friends.include?(user)
+  end
 
 end
