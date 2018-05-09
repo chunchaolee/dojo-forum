7acd751de0f9ce7a1c4963a382ed2238039e5931
chunchaolee <chunchaolee@gmail.com>
2018-05-09 19:56:11 +0800

    setup counter_cache for user replies_count

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,7 +3,7 @@ class Reply < ApplicationRecord
   validates_presence_of :comment
 
   # 一筆reply只屬於一個user
-  belongs_to :user
+  belongs_to :user, :counter_cache => true
 
   # 一筆reply只屬於一個post
   belongs_to :post, :counter_cache => true
