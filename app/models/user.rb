6b56a9f0e3fba84a30a41f00d2b3d606ee5bdec8
chunchaolee <chunchaolee@gmail.com>
2018-05-11 08:41:21 +0800

    setup generate_authentication_token method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  before_create :generate_authentication_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -40,6 +42,10 @@ class User < ApplicationRecord
 
   def is_applied?(user)
     self.friends.include?(user)
+  end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
   end
 
 end
