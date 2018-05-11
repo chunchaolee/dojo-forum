4c0a3daffe432791263d84dd148cd4ea3c55550b
chunchaolee <chunchaolee@gmail.com>
2018-05-11 08:41:46 +0800

    added token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509043158) do
+ActiveRecord::Schema.define(version: 20180511003517) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20180509043158) do
     t.text "description"
     t.string "avatar"
     t.integer "replies_count"
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
