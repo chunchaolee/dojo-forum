448cdd56b81693e89bb2c682249efb29606f01f4
chunchaolee <chunchaolee@gmail.com>
2018-05-12 21:53:08 +0800

    remove column category_ids in posts table

--- db/migrate/20180429034954_rename_column_post_categories_on_post.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnPostCategoriesOnPost < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :posts, :post_categories, :category_ids
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512133935) do
+ActiveRecord::Schema.define(version: 20180512134909) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20180512133935) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_ids", default: 0
     t.boolean "is_published", default: true
     t.datetime "last_replied_at"
     t.index ["replies_count"], name: "index_posts_on_replies_count"
