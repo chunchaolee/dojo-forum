3c92fcf3c871968278684bb1f137face6783f316
chunchaolee <chunchaolee@gmail.com>
2018-05-12 23:32:05 +0800

    added if column_exists? to add_column_category_ids_on_post

--- db/migrate/20180512152808_add_column_category_ids_on_post.rb
+++ db/migrate/20180512152808_add_column_category_ids_on_post.rb
@@ -1,5 +1,11 @@
 class AddColumnCategoryIdsOnPost < ActiveRecord::Migration[5.1]
   def change
+
+    if column_exists? :posts, :category_ids
+      remove_column :posts, :category_ids
+    end
+
     add_column :posts, :category_ids, :integer, using: 'category_ids::integer[]'
+
   end
 end
